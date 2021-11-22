f4312e4c6ed5ae51218de61a2505093fbf7b5568
Jack Garratt <jack.garratt@icfnext.com>
2021-11-22 16:37:38 -0500

    Continued styling and some basic html scaffolding

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import './CharacterCard.css';
+import React from 'react';
 
 const CharacterCard = () => {
   return (
-    <h3>Character Card</h3>
+    <div className="ui raised very text segment character-card">
+      <div className="character-card--attribute">
+        <span><strong>Initiative:</strong></span>
+        <span>18</span>
+      </div>
+      <div className="character-card--attribute">
+        <span><strong>Character Name:</strong></span>
+        <span>Applebottom</span>
+      </div>
+      <div className="character-card--attribute">
+        <span><strong>HP:</strong></span>
+        <span>40</span>
+      </div>
+      <div className="character-card--attribute">
+        <span><strong>Notes:</strong></span>
+        <p>Lorem Ipsum</p>
+      </div>
+    </div>
   );
 }
 
--- src/components/InitiativeList.js
+++ src/components/InitiativeList.js
@@ -1,14 +1,22 @@
+import './InitiativeList.css';
 import React from 'react';
 import CharacterCard from './CharacterCard';
 
 const InitiativeList = () => {
   return (
-    <div className="ui form raised very padded text segment">
+    <div className="ui raised very padded text segment">
       <h2>Initiative Board</h2>
       <CharacterCard />
       <CharacterCard />
       <CharacterCard />
       <CharacterCard />
+      <div className="button_container">
+        <div>
+          <button className="ui button">Previous Player</button>
+          <button className="ui button">Next Player</button>
+        </div>
+        <button className="ui red button">Clear Board</button>
+      </div>
     </div>
   );
 }
